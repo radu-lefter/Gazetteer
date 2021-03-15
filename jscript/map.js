73a712f59390ff6bfa8aa867298c4bdc955b9719
radu-lefter <68811789+radu-lefter@users.noreply.github.com>
2021-03-15 08:46:37 +0000

    added error control to news api data

--- jscript/map.js
+++ jscript/map.js
@@ -1,8 +1,3 @@
-//Preloader
-// $(window).on('load', function () { 
-//        if ($('#preloader').length) { 
-//                 $('#preloader').delay(100).fadeOut('slow', function () {        $(this).remove();      });    }});
-
 
 //populate the select list
 
@@ -14,29 +9,7 @@ dropdown.append('<option selected="true" value="dummy" disabled>Choose country</
 dropdown.prop('selectedIndex', 0);
 
 
-//let countries = [];
-
-    // $.ajax({
-    //     dataType: "json",
-    //     url: "php/getCountriesData.php",
-    //     success: function (data) {
-
-            
-    //             $.each(data.features, function (key, country) {
-    //                 countries.push({"name": country.properties.name, "iso": country.properties.iso_a3}) 
-    //             })
-                
-    //             countries.sort((a, b) => (a.name > b.name) ? 1 : -1);
-
-    //              for (let item of countries) {
-    //                 dropdown.append($('<option></option>').attr('value', item.iso).text(item.name));
-    //             }
-           
-    //     },
-    // }).fail(function () {});
-
 $.ajax({
-        //dataType: "json",
         type: 'GET',
         url: "php/getCountriesData.php",
         success: function (response) {
@@ -96,7 +69,6 @@ function selectCountry(country, country_iso) {
     console.log(country);
     console.log(country_iso);
     $.ajax({
-        //dataType: "json",
         url: "php/getCountriesBorders.php",
         success: function (response) {
 
@@ -151,17 +123,13 @@ function selectCountry(country, country_iso) {
                         $('#weather').html("Temperature: " + result['data']['weather']['main']['temp'] + "&#8451; " + result['data']['weather']['weather'][0]['description']);
                         $('#wiki').html(result['data']['wiki']['extract']);
                         $('#photoImg').attr({src: result['data']['photo']['results'][rand_photo]['urls']['small']});
-                        $('#news1').attr({href: result['data']['news']['articles'][rand_news1]['url']});
-                        $('#news1').html(result['data']['news']['articles'][rand_news1]['title']);
-                        $('#news2').attr({href: result['data']['news']['articles'][rand_news2]['url']});
-                        $('#news2').html(result['data']['news']['articles'][rand_news2]['title']);
-                        $('#news3').attr({href: result['data']['news']['articles'][rand_news3]['url']});
-                        $('#news3').html(result['data']['news']['articles'][rand_news3]['title']);
+                        
                         if(result['data']['youtube']['items']){
                             $('#iframe').attr({src: 'https://www.youtube.com/embed/'+ result['data']['youtube']['items'][0]['id']['videoId']});
                         } else {
                             $('#video').html("Video not found");
                         }
+
                         if(result['data']['covid']["cases"]){
                         $('#cases').html("Total cases: " + result['data']['covid']["cases"]);
                         $('#recovered').html("Total recovered: " + result['data']['covid']["recovered"]);
@@ -173,13 +141,22 @@ function selectCountry(country, country_iso) {
                             $('#deaths').html("Sorry, no data found");
                             $('#deathsToday').html("Sorry, no data found");
                         }
+
+                        if(result['data']['news']['articles']){
+                            $('#news1').attr({href: result['data']['news']['articles'][rand_news1]['url']});
+                            $('#news1').html(result['data']['news']['articles'][rand_news1]['title']);
+                            $('#news2').attr({href: result['data']['news']['articles'][rand_news2]['url']});
+                            $('#news2').html(result['data']['news']['articles'][rand_news2]['title']);
+                            $('#news3').attr({href: result['data']['news']['articles'][rand_news3]['url']});
+                            $('#news3').html(result['data']['news']['articles'][rand_news3]['title']);
+                        }else{
+                            $('#news').html("Sorry, news service not available at the moment.")
+                        }
+                        
                     }
-        
-                    
-                
+  
                 },
                 error: function(jqXHR, textStatus, errorThrown) {
-                    // your error code
                     console.log("An error has occured!");
                 }
             }); 
@@ -190,96 +167,3 @@ function selectCountry(country, country_iso) {
     }).fail(function () {});
 };
 
-
-
-//alternative of having every country clickable
-
-// $.ajax({
-//     dataType: "json",
-//     url: "countryBorders.geo.json",
-//     success: function (data) {
-//         //console.log(data);
-//         L.geoJson(data).addTo(mymap);
-
-
-//         var geojson;
-        
-//         function highlightFeature(e) {
-//             var layer = e.target;
-        
-//             layer.setStyle({
-//                 weight: 5,
-//                 color: '#f4c324',
-//                 dashArray: '',
-//                 fillOpacity: 0.1
-//             });
-        
-//             if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
-//                 layer.bringToFront();
-//             }
-//         }
-        
-//         function resetHighlight(e) {
-//             geojson.resetStyle(e.target);
-//         }
-        
-//         function getCountryInfo(e) {
-//             mymap.fitBounds(e.target.getBounds()); // zoom to feature
-//             console.log(e.target);
-           
-
-//             //populate modal with country info
-//             $.ajax({
-//                 url: "jscript/api.php",
-//                 type: 'POST',
-//                 dataType: 'json',
-//                 data: {
-//                     country: e.target.feature.properties.iso_a3
-//                 },
-//                 success: function(result) {
-    
-//                     //console.log(result);
-    
-//                     if (result.status.name == "ok") {
-    
-//                         $('#countryName').html(result['data']["name"]);
-//                         $('#capital').html("Capital: " + result['data']["capital"]);
-//                         $('#continent').html("Continent: " + result['data']["subregion"]);
-//                         $('#population').html("Population: " + result['data']["population"]);
-//                         $('#language').html("Language: " + result['data']["languages"][0]["name"]);
-//                         $('#currency').html("Currency: " + result['data']["currencies"][0]["name"]);
-//                         $('#flagImg').attr({src: result['data']['flag'], style: "width:30px"});
-                        
-                        
-//                     }
-                
-//                 },
-//                 error: function(jqXHR, textStatus, errorThrown) {
-//                     // your error code
-//                     console.log("An error has occured!");
-//                 }
-//             }); 
-
-        
-
-//             $('#myModal').modal('show');
-
-//         }
-
-        
-
-        
-//         function onEachFeature(feature, layer) {
-//             layer.on({
-//                 mouseover: highlightFeature,
-//                 mouseout: resetHighlight,
-//                 click: getCountryInfo
-                
-//             });
-//         }
-        
-//         geojson = L.geoJson(data, {
-//             onEachFeature: onEachFeature
-//         }).addTo(mymap);
-//     },
-//   }).fail(function () {});
